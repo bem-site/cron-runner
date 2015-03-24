02dd5b2c15e811446707a4fb9ffe0d9d3f3c7073
tormozz48 <tormozz48@gmail.com>
2015-03-24 15:25:38 +0400

    Add test folder

---
+++ test/index.js
@@ -0,0 +1,11 @@
+var should = require('should');
+
+describe('bem-data-source', function () {
+    before(function () {});
+
+    it('should be ok', function () {
+        console.log('ok');
+    });
+
+    after(function () {});
+});
